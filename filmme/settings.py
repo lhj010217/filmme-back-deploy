4d9c594413ffdc0898438a17731df709263cc020
leehyungjoon <eslee1777@naver.com>
2024-06-13 01:19:51 +0900

    commit

--- filmme/settings.py
+++ filmme/settings.py
@@ -187,9 +187,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 
